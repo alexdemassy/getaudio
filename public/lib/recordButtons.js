5b3d204a15e385fefa20e7d0cee1a0f38eabac65
Mathieu Larocque <Mathieu.Larocque@kandy.io>
2016-09-26 12:22:15 -0400

    removed extra html

--- public/lib/recordButtons.js
+++ public/lib/recordButtons.js
@@ -8,12 +8,15 @@ class Hello extends React.Component {
     }
     render() {
         var { recording, blob } = this.state;
-        return ( <div className='controls'>
+        return ( 
+        <div className='get-audio'>
             <audio ref='playback' src={blob} controls='true'>Browser does not support HTML5 audio element.</audio>
-            <button ref="start" onClick={() => startRecording(this)} disabled={recording}>Start</button>
-            <button ref="stop" onClick={() => stopRecording(this)} disabled={!recording}>Stop</button>
-            <button ref="upload" onClick={() => uploadBlob(this)} disabled={blob ? false : true}>Upload</button>
-            <button ref="download" onClick={() => downloadBlob(this)} disabled={blob ? false : true}>Download</button>
+            <div className='controls'>
+                <button ref="start" onClick={() => startRecording(this)} disabled={recording}>Start</button>
+                <button ref="stop" onClick={() => stopRecording(this)} disabled={!recording}>Stop</button>
+                <button ref="upload" onClick={() => uploadBlob(this)} disabled={blob ? false : true}>Upload</button>
+                <button ref="download" onClick={() => downloadBlob(this)} disabled={blob ? false : true}>Download</button>
+            </div>
         </div>)
     }
 }
